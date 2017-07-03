749570992e18f9771a1ded0ff9492e368bf4e7cd
Noah Rosenzweig <noah@rays-lab.com>
2017-07-03 18:30:51 +0200

    added a couple options; this commit is mainly for easy use with my i3 status bar with my configuration

--- timer.sh
+++ timer.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 defPath="/home/noah/Projects/shell/timer"
-defCmd="cvlc"
-cmd=$defCmd
+defCmd="cvlc --play-and-exit -q"
+cmd="$defCmd"
 defAlarm="$defPath/alarm.mp3"
-alarm=$defAlarm
+alarm="$defAlarm"
 helpFile="$defPath/help.txt"
 Opts=("$@")
 format="m.s"
@@ -22,51 +22,100 @@ i3statusFormat="Timer: %m:%s"
 i3icons=( "" "" )
 i3iconCurFront=0
 i3iconCurBack=1
+newOpts=
+bgOutputPath="/home/noah/.remaining_time"
+bgSetOutput=
+popoutCmd="gnome-terminal --role TIMER -x bash -c echo\ Time\"\'\"s\ Up!\;\ \"$cmd\"\ \"$alarm\""
+inBg=
+
 
 if [ $# -gt 0 ]; then
 
 	# get options
 	for (( count = 0; count < $#; count++ )); do
-		opt=${Opts[$count]}
+		opt="${Opts[$count]}"
 
 		if [ -n "${Opts[$count + 1]}" ]; then
-			val=${Opts[$count + 1]}
+			val="${Opts[$count + 1]}"
 		fi
 
-		if [ ${opt:0:1} == "-" ]; then
+		if [ "${opt:0:1}" == "-" ]; then
 			if [[ ${opt:1:1} == "h" ]]; then  # help
 				cat $helpFile
 				exit
-			elif [[ ${opt:1:1} == "f" ]]; then  # format
-				format=$val
-			elif [[ ${opt:1:1} == "o" ]]; then  # output format
-				outFormat=$val
-			elif [[ ${opt:1:1} == "t" ]]; then  # in
-				time=$val
-			elif [[ ${opt:1:1} == "w" ]]; then  # wait / sleep duration
-				sleepDur=$val
-			elif [[ ${opt:1:1} == "C" ]]; then  # custom command
-				cmd=$val
-			elif [[ ${opt:1:1} == "A" ]]; then  # custom audio file
+			elif [[ "${opt:1:1}" == "f" ]]; then  # format
+				format="$val"
+			elif [[ "${opt:1:1}" == "o" ]]; then  # output format
+				outFormat="$val"
+			elif [[ "${opt:1:1}" == "t" ]]; then  # in
+				time=$( echo -n "$val" | tr -d "\"" )
+			elif [[ "${opt:1:1}" == "w" ]]; then  # wait / sleep duration
+				sleepDur="$val"
+			elif [[ "${opt:1:1}" == "C" ]]; then  # custom command
+				cmd="$val"
+			elif [[ "${opt:1:1}" == "A" ]]; then  # custom audio file
 				if [ "$cmd" != "$defCmd" ]; then
 					echo -e "setting a custom audio file has no effect in conjunction with using a custom command\nignoring custom audio file"
 					read -n1
-				elif [[ -f "$val" && ( $val == *".mp3" || $val == *".wav" || $val == *".m4a" ) ]]; then
-					alarm=$val
+				elif [[ -f "$val" && ( "$val" == *".mp3" || "$val" == *".wav" || "$val" == *".m4a" ) ]]; then
+					alarm="$val"
 				else
 					echo "$val is not a file, using default alarm ($defAlarm)"
 					read -n1
 				fi
-			elif [[ ${opt:1:1} == "m" ]]; then  # display milliseconds
+			elif [[ "${opt:1:1}" == "m" ]]; then  # display milliseconds
 				milli=$val
-			elif [[ ${opt:1:1} == "O" ]]; then  # set output path
+			elif [[ "${opt:1:1}" == "O" ]]; then  # set output path
 				output=$val
-			elif [[ ${opt:1:1} == "B" ]]; then
+			elif [[ "${opt:1:1}" == "B" ]]; then  # display remaining time in i3 status bar (with my config)
 				i3statusOutput=true
 				output="$i3statusPath"
+				inBg=true
+			elif [[ "${opt:1:1}" == "b" ]]; then  # restart timer in background
+				bgSetOutput=true
+				inBg=true
+			elif [[ "${opt:1:1}" == "P" ]]; then  # create new terminal window with alarm playing when timer finishes (popout)
+				cmd="$popoutCmd"
 			fi
 		fi
 	done
+
+	# set output path to i3 status bar custom output (mainly for starting in bg, workaround)
+	if [ "$output" == "$i3statusPath" ]; then
+		i3statusOutput=true
+	fi
+
+	# set output path if non set and if timer should restart in bg
+	if [[ -n "$bgSetOutput" && "$output" == "$defOutput" ]]; then
+		newOpts[${#newOpts[@]}]="-O"
+		newOpts[${#newOpts[@]}]="$bgOutputPath"
+	fi
+
+	# restart in bg
+	if [ -n "$inBg" ]; then
+		finalOpts=()
+		# check for -b or -B opts in newOpts to avoid infinite application starting recursion
+		for (( count = 0; count < ${#Opts[@]}; count++ )); do
+			skip=false
+			if [[ "${Opts[$count]:0:1}" == "-" ]]; then
+				if [[ "${Opts[$count]:1:1}" != "b" && "${Opts[$count]:1:1}" != "B" ]]; then
+					finalOpts[${#finalOpts[@]}]="${Opts[$count]}"
+				fi
+				skip=true
+			fi
+			if [ "$skip" == "false" ]; then
+				finalOpts[${#finalOpts[@]}]="${Opts[$count]}"
+			fi
+
+		done
+		extraOpts=
+		if [ -n "$i3statusOutput" ]; then
+			extraOpts=("-O" "$i3statusPath" "-P")
+		fi
+		# restart script
+		${defPath}/timer.sh "${finalOpts[@]}" "${extraOpts[@]}" &
+		exit
+	fi
 
 
 	if [ -z "$outFormat" ]; then
@@ -77,7 +126,6 @@ if [ $# -gt 0 ]; then
 		else
 			# else use format as output
 			outFormat=$format
-			#outFormat="%m:%s"
 		fi
 
 	fi
@@ -85,25 +133,25 @@ if [ $# -gt 0 ]; then
 
 	# create timer with options
 	# compare $time with $format
-	timeArr=($(echo $time | sed 's/\([ [:punct:]]\)/ \1 /g'))
+	timeArr=($( echo -n "$time" | sed 's/\([ [:punct:]]\)/ \1 /g' ))
 	for (( count = 0; count < ${#timeArr[@]}; count++ )); do
 		inCur=${timeArr[$count]}
-		fCur=${format:$count:1}
+		fCur="${format:$count:1}"
 
 		case "$fCur" in
 			# TIME:
 			"s")  # seconds
-				if [ "$inCur" -eq "$inCur" ]; then
+				if [ $inCur -eq $inCur ]; then
 					timeSec=$(echo "$timeSec + $inCur" | bc)
 				fi
 				;;
 			"m")  # minutes
-				if [ "$inCur" -eq "$inCur" ]; then
+				if [ $inCur -eq $inCur ]; then
 					timeSec=$(echo "$timeSec + ($inCur * 60)" | bc)
 				fi
 				;;
 			"h")  # hours
-				if [ "$inCur" -eq "$inCur" ]; then
+				if [ $inCur -eq $inCur ]; then
 					timeSec=$(echo "$timeSec + ($inCur * 3600)" | bc)
 				fi
 				;;
@@ -113,7 +161,7 @@ if [ $# -gt 0 ]; then
 	done
 
 	# execute timer
-	if [ $execTimer ]; then
+	if [ -n "$execTimer" ]; then
 		startTime=$(date +%s)
 		endTime=$(echo "$startTime + $timeSec" | bc)
 		while [ $(date +%s) -lt $endTime ]; do
@@ -145,7 +193,7 @@ if [ $# -gt 0 ]; then
 
 			# necessary to print remaining time in $outFormat
 			for (( countF = 0; countF < ${#outFormat}; countF++ )); do
-				cur=${outFormat:$countF:1}
+				cur="${outFormat:$countF:1}"
 				if [[ "$format" == "$outFormat" || "$cur" == "%" ]]; then
 					caseCur=
 					if [ "$cur" == "%" ]; then
@@ -169,7 +217,7 @@ if [ $# -gt 0 ]; then
 						*)
 							echo -n "$cur" >> $output
 					esac
-				elif [[ "${outFormat:$countF-1:1}" != "%" ]]; then
+				elif [[ "${outFormat:${countF}-1:1}" != "%" ]]; then
 					echo -n "$cur" >> $output
 				fi
 			done
@@ -177,7 +225,7 @@ if [ $# -gt 0 ]; then
 			if [[ "$outFormat" != *"s"* ]]; then
 				echo -n ".$sec s" >> $output
 			fi
-			if [[ $milli != "" ]]; then
+			if [[ "$milli" != "" ]]; then
 				echo -n " ,$(date +%N | cut -c1-$milli) ms" >> $output
 			fi
 
@@ -194,12 +242,12 @@ if [ $# -gt 0 ]; then
 		done
 
 		if [ "$output" == "$defOutput" ]; then clear; fi
+		echo "Time's Up! ($time)" > $output
 		if [ "$cmd" == "$defCmd" ]; then
-			echo "Time's Up!" > $output
 			$cmd "$alarm"
-			echo -n "" > $output
 		else
 			bash -c "$cmd" >> $output
 		fi
+		#echo -n "" > $output
 	fi
 fi
